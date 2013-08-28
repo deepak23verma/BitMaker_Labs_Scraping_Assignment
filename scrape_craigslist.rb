5f31ef57e3c56c87676112830f601fe83158734b
Deepak Verma <deepakverma@Deepaks-MacBook-Pro.local>
2013-08-28 13:47:49 -0400

    craigslist scraping

---
+++ scrape_craigslist.rb
@@ -0,0 +1,16 @@
+require_relative 'rubygems'
+require_relative 'nokogiri'
+require_relative 'open­uri'
+
+page_craigslist = "http://toronto.en.craigslist.ca/search/?areaID=25&subAreaID=&query=macbook+pro&catAbb=sss"
+page = Nokogiri::HTML(open(page_craigslist))
+links = page.css("a")
+puts links["href"]
+
+# File.open('macbook.html', 'w') do |f|
+# 	puts links["href"]
+# 	books = page.css('table tr td:first­child span')
+
+# 	books.each do |book|
+# 		f.write(“<div>” + book + "</div>\n")
+# 	end
