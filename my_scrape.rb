f64df6b1c362ccccdf3ce7d24edfaa4d6ebc426b
kimyee <kimyee.16@gmail.com>
2013-08-29 19:06:57 -0400

    pusing css updates

--- my_scrape.rb
+++ my_scrape.rb
@@ -20,6 +20,7 @@ File.open("my_scrape.html", "w") do |line|
 	line.puts("\t<link rel=\"stylesheet\" type=\"text/css\" href=\"reset.css\">")
 	line.puts("\t<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">")
 	line.puts("<link href=\'http://fonts.googleapis.com/css?family=Carrois+Gothic\' rel=\'stylesheet' type=\'text/css\'>")
+	line.puts("<link href=\'http://fonts.googleapis.com/css?family=Mouse+Memoirs\' rel=\'stylesheet' type=\'text/css'\>")
 	line.puts("\t<title>Reddit 2.0</title>")
 	line.puts("</head>")
 	####################################################################################
@@ -32,13 +33,14 @@ File.open("my_scrape.html", "w") do |line|
 			line.puts("</div>") #closing header div
 			line.puts("<div id=\"headline\">")
 				line.puts("<a href=\"#{reddit_links[0].css('a.title').first.attributes["href"].value}\">#{reddit_links[0].css('a.title').first.text}</a>")
-			line.puts("</div>") #close headline div
+				line.puts("<p><img src=\"#{reddit_links[0].css('img').first.attributes["src"].value}\"></p>") unless reddit_links[0].css('img').first.nil?
+ 			line.puts("</div>") #close headline div
 			line.puts("<div class=\"secondary\">")
 				reddit_links.each_with_index do |link, index|
 					if index >0 
 					line.puts("<div class=\"second_row_news\">")
 					line.puts("<a href=\"#{link.css('a.title').first.attributes["href"].value}\">#{link.css('a.title').first.text}</a>")
-					line.puts("<img src=\"#{link.css('img').first.attributes["src"].value} \">") unless link.css('img').first.nil?
+					line.puts("<p><img src=\"#{link.css('img').first.attributes["src"].value} \"></p>") unless link.css('img').first.nil?
 					line.puts("</div>")
 					end
 				end
