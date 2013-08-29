3e8560286039ddb0bbc00985bf374316885cd60e
kimyee <kimyee.16@gmail.com>
2013-08-29 17:58:44 -0400

    git commit all day is pulling from reddit

--- my_scrape.rb
+++ my_scrape.rb
@@ -4,7 +4,13 @@ require "open-uri"
 
 PAGE_URL = "http://www.reddit.com/"
 page = Nokogiri::HTML(open(PAGE_URL))
-reddit_links = page.css("html body.listing-page.hot-page div.content div.spacer div#siteTable.sitetable.linklisting a.title")
+reddit_links = page.css("html body.listing-page.hot-page div.content div.spacer div#siteTable.sitetable.linklisting div.link")
+# reddit_links = page.css("html body.listing-page.hot-page div.content div.spacer div#siteTable.sitetable.linklisting a.title")
+# reddit_images = page.css("html body.listing-page.hot-page div.content div.spacer div#siteTable.sitetable.linklisting img")
+#reddit_comments = page.css("")
+p reddit_links.length
+p reddit_links.css('a.title').first.attributes["href"].value
+p reddit_links.css('img').first.attributes["src"].value
 
 
 File.open("my_scrape.html", "w") do |line|
@@ -25,26 +31,36 @@ File.open("my_scrape.html", "w") do |line|
 				line.puts("<h1>A More Awesome Reddit</h1>")
 			line.puts("</div>") #closing header div
 			line.puts("<div id=\"headline\">")
-				line.puts("<a href=\"#{reddit_links[0]['href']}\">#{reddit_links[0].text}</a>")
+				line.puts("<a href=\"#{reddit_links[0].css('a.title').first.attributes["href"].value}\">#{reddit_links[0].css('a.title').first.text}</a>")
 			line.puts("</div>") #close headline div
 			line.puts("<div class=\"secondary\">")
-				line.puts("<div class=\"second_row_news\">")
-					line.puts("something1")
-				line.puts("</div>") #close second_row_news div
-				line.puts("<div class=\"second_row_news\">")
-					line.puts("something2")
-				line.puts("</div>") #close second_row_news div
-				line.puts("<div class=\"second_row_news\">")
-					line.puts("something3")
-				line.puts("</div>") #close second_row_news div
+				reddit_links.each_with_index do |link, index|
+					if index >0 
+					line.puts("<div class=\"second_row_news\">")
+					line.puts("<a href=\"#{link.css('a.title').first.attributes["href"].value}\">#{link.css('a.title').first.text}</a>")
+					line.puts("<img src=\"#{link.css('img').first.attributes["src"].value} \">") unless link.css('img').first.nil?
+					line.puts("</div>")
+					end
+				end
+				# line.puts("<div class=\"second_row_news\">")
+				# 	line.puts("something1")
+				# line.puts("</div>") #close second_row_news div
+				# line.puts("<div class=\"second_row_news\">")
+				# 	line.puts("something2")
+				# line.puts("</div>") #close second_row_news div
+				# line.puts("<div class=\"second_row_news\">")
+				# 	line.puts("something3")
+				# line.puts("</div>") #close second_row_news div
 			line.puts("</div>") #close secondary div
+			line.puts("<div id=\"footer\">")
+				line.puts("footer")
 		# reddit_links.each do |link|
 		# 	line.puts("<a href=\"#{link['href']}\">#{link.text}</a>")
 		# 	end
 	####################################################################################
 	####################################################################################
 	####################################################################################
-		
+	line.puts("</div>") #closing footer div	
 	line.puts("</div>") #closing container div
 	line.puts("</body>")
 	line.puts("</html>")
